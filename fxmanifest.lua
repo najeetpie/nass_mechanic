9dddbf5d515b0dc1bd409c054c5a6146211da232
Naji <104698566+najeetpie@users.noreply.github.com>
2023-04-07 16:48:20 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ use_experimental_fxv2_oal 'yes'
 game 'gta5'
 lua54 'yes'
 
-description 'nass_launchcontrol'
+description 'nass_mechanic'
 author 'Nass#1411'
 version '1.0.0'
 
@@ -12,8 +12,3 @@ shared_scripts { 'config.lua' }
 server_scripts { 'bridge/**/server.lua', 'server/*.lua' }
 
 client_scripts { 'bridge/**/client.lua', 'client/*.lua' }
-
-ui_page 'html/index.html'
-files { 
-  "html/*",
-}
