a3dc8a1f559a8c124bd0dfdbe4641286f6b65b73
Naji <104698566+najeetpie@users.noreply.github.com>
2023-04-14 01:53:42 -0400

    New Blip Configs

--- config.lua
+++ config.lua
@@ -1,14 +1,14 @@
 Config = {}
 
 Config.PedList = {
-	{ped = 's_m_y_xmech_01', name = "Victor", pos = vector4(-205.9580, -1327.3136, 29.8904, 23.6360)},
-	{ped = 's_m_y_xmech_01', name = "Pedro", pos = vector4(540.1721, -173.8154, 53.4813, 138.2029)},
-	{ped = 's_m_y_xmech_02', name = "Tony", pos = vector4(-336.8208, -141.5550, 38.0096, 40.6875)},
-	{ped = 's_m_y_xmech_01', name = "Nass", pos = vector4(-1161.1299, -2006.3768, 12.1803, 287.2793)},
-	{ped = 's_m_y_xmech_02', name = "Rodriguez", pos = vector4(736.0736, -1081.4657, 21.1687, 131.7041)},
-	{ped = 's_m_y_xmech_01', name = "Miguel", pos = vector4(1177.1703, 2637.2473, 36.7538, 25.0721)},
-	{ped = 's_m_y_xmech_02', name = "Rayyan", pos = vector4(106.6484, 6627.4414, 30.7873, 257.3856)},
-	{ped = 's_m_y_xmech_02', name = "Gabe", pos = vector4(1152.3156, -776.8295, 56.5987, 48.9483)},
+	{ped = 's_m_y_xmech_01', name = "Victor", pos = vector4(-205.9580, -1327.3136, 29.8904, 23.6360), blip = {enabled = true, sprite = 402, scale = 0.75, label = "Mechaic", shortRange = true}},
+	{ped = 's_m_y_xmech_01', name = "Pedro", pos = vector4(540.1721, -173.8154, 53.4813, 138.2029), blip = {enabled = true, sprite = 402, scale = 0.75, label = "Mechaic", shortRange = true}},
+	{ped = 's_m_y_xmech_02', name = "Tony", pos = vector4(-336.8208, -141.5550, 38.0096, 40.6875), blip = {enabled = true, sprite = 402, scale = 0.75, label = "Mechaic", shortRange = true}},
+	{ped = 's_m_y_xmech_01', name = "Nass", pos = vector4(-1161.1299, -2006.3768, 12.1803, 287.2793), blip = {enabled = true, sprite = 402, scale = 0.75, label = "Mechaic", shortRange = true}},
+	{ped = 's_m_y_xmech_02', name = "Rodriguez", pos = vector4(736.0736, -1081.4657, 21.1687, 131.7041), blip = {enabled = true, sprite = 402, scale = 0.75, label = "Mechaic", shortRange = true}},
+	{ped = 's_m_y_xmech_01', name = "Miguel", pos = vector4(1177.1703, 2637.2473, 36.7538, 25.0721), blip = {enabled = true, sprite = 402, scale = 0.75, label = "Mechaic", shortRange = true}},
+	{ped = 's_m_y_xmech_02', name = "Rayyan", pos = vector4(106.6484, 6627.4414, 30.7873, 257.3856), blip = {enabled = true, sprite = 402, scale = 0.75, label = "Mechaic", shortRange = true}},
+	{ped = 's_m_y_xmech_02', name = "Gabe", pos = vector4(1152.3156, -776.8295, 56.5987, 48.9483), blip = {enabled = true, sprite = 402, scale = 0.75, label = "Mechaic", shortRange = true}},
 }
 
 Config.price = 1000.0
